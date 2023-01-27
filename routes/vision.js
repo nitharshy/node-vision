24f705775e77a97301364a64d13ef82a335e9898
Nisha <nitharshykathir@gmail.com>
2023-01-27 13:02:46 +0530

    adding aws rekognision api

--- routes/vision.js
+++ routes/vision.js
@@ -3,15 +3,41 @@ var router = express.Router();
 
 router.post('/classify', function(req, res, next) {
   // DON'T return the hardcoded response after implementing the backend
-  let response = ["shoe", "red", "nike"];
 
   // Your code starts here //
+  let response = [];
+
+
+  var AWS = require('aws-sdk');
+  AWS.config.update({
+    accessKeyId: 'AKIARAR74F5B2ZJFROOU',
+    secretAccessKey: '58t6FYfBVhi0FhEKFwxOWExsgASY3dtg6EHAPcVP',
+    region: 'ap-southeast-1'
+  });
+  const client = new AWS.Rekognition();
+  const params = {
+    Image: {
+      Bytes: req.files.file.data
+    },
+    MaxLabels: 10
+  }
+  client.detectLabels(params, function (err, resp) {
+    if (err) {
+      console.log(err, err.stack); // if an error occurred
+    } else {
+      // console.log(`Detected labels for: `)
+      console.log(resp.Labels);
+      resp.Labels.forEach(label => {
+        response.push(label.Name);
+      }) // for response.labels
+
+      res.json({
+        "labels": response
+      });
+    } // if
+  });
 
   // Your code ends here //
-
-  res.json({
-    "labels": response
-  });
 });
 
 module.exports = router;
